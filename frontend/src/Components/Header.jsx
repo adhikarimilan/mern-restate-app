158c53e21a8576fca893f5b3cdfaa2b95e242073
adhikarimilan <38108519+adhikarimilan@users.noreply.github.com>
2024-11-23 18:59:23 +1100

    added header

---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="bg-slate-200 shadow-md p-3">
+      <div className="flex justify-between items-center max-w-6xl mx-auto">
+        <Link to={"/"}>
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Milan</span>{" "}
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex flex-row items-center">
+          <input
+            type="text"
+            placeholder="Search &#8230;"
+            className="bg-transparent w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline">Home</li>
+          </Link>
+          <Link to={"/about"}>
+            <li className="hidden sm:inline">About</li>
+          </Link>
+          <Link to={"/signin"}>
+            <li>Signin</li>
+          </Link>
+          <Link to={"/signup"}>
+            <li>Signup</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
