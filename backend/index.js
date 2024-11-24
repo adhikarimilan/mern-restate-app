5c3edbd3fa431e01b4fb0fdc195a66a83fb6cac9
adhikarimilan <38108519+adhikarimilan@users.noreply.github.com>
2024-11-24 18:38:57 +1100

    installed mongoose and connected to the db

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000!");
+app.listen(process.env.SERVER_PORT, () => {
+  mongoose
+    .connect(process.env.MongoDBUrl)
+    .then(() => {
+      console.log("Connected to the Database");
+    })
+    .catch((error) => {
+      console.error(`An error Occured while connected to the db: ${error}}`);
+    });
+  console.log(`server is running on port ${process.env.SERVER_PORT}`);
 });
