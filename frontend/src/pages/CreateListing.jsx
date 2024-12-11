f669ad847d403f94a85ba07e159425688001481b
adhikarimilan <38108519+adhikarimilan@users.noreply.github.com>
2024-12-12 01:57:01 +1100

    completed image upload frontend

--- frontend/src/pages/CreateListing.jsx
+++ frontend/src/pages/CreateListing.jsx
@@ -1,6 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { Link } from "react-router-dom";
+import { app } from "../firebase.js";
 
 const CreateListing = () => {
+  const [files, setFiles] = useState([]);
+  const [uploading, setUploading] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  useEffect(() => {
+    console.log(formData);
+  }, [formData.imageUrls]);
+
+  //handling image submit
+  const handleImageSubmit = (e) => {
+    e.preventDefault();
+    if (files.length > 0 && files.length < 7) {
+      setUploading(true);
+      const promises = [];
+
+      for (let i = 0; i < files.length; i++) {
+        promises.push(uploadImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+        })
+        .finally(() => {
+          setUploading(false);
+        }); // Ensures loading is set to false after all uploads finish.
+    }
+  };
+
+  //uploading each image indivually
+  const uploadImage = async (file) => {
+    try {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name.replace(/\s/g, "_");
+
+        const storageRef = ref(storage, fileName);
+        const uploadFile = uploadBytesResumable(storageRef, file);
+
+        uploadFile.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setUploadProgress(Math.round(progress));
+          },
+          (error) => {
+            console.error(`An error occured while uploading: ${error}`);
+          },
+          () => {
+            getDownloadURL(uploadFile.snapshot.ref).then(
+              async (downloadUrl) => {
+                resolve(downloadUrl);
+              }
+            );
+          }
+        );
+      });
+    } catch (error) {
+      console.error(`An error occured: ${error}`);
+    }
+  };
+
+  const removeUploadedImage = (index) => {
+    console.log(index);
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
+  };
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -143,12 +226,42 @@ const CreateListing = () => {
               id="images"
               accept="image/*"
               multiple
+              onChange={(e) => {
+                setFiles(Array.from(e.target.files)); // Convert FileList to array
+              }}
               className="p-2 border border-gray-300 rounded w-full"
             />
-            <button className="border p-3 border-green-600 bg-green-600 hover:opacity-90 disabled:opacity-80 text-white rounded uppercase">
-              Upload
+            <button
+              className="border p-3 border-green-600 bg-green-600 hover:opacity-90 disabled:opacity-80 text-white rounded uppercase"
+              onClick={handleImageSubmit}
+            >
+              {uploading ? "Uploading..." : "Upload"}
             </button>
           </div>
+
+          {formData.imageUrls.length > 0 ? (
+            <div className="flex flex-col p-2 gap-2">
+              {formData.imageUrls.map((url, index) => (
+                <div
+                  key={url}
+                  className="flex justify-between items-center gap-2"
+                >
+                  <img
+                    src={url}
+                    alt={"uploaded_img_" + (index + 1)}
+                    className="w-20 h-20 object-contain rounded-lg"
+                  />
+                  <AiOutlineClose
+                    className="text-red-600 font-bold hover:opacity-90"
+                    onClick={() => removeUploadedImage(index)}
+                  />
+                </div>
+              ))}
+            </div>
+          ) : (
+            <></>
+          )}
+
           <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
             Create Listing
           </button>
